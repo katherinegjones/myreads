817dfd86fe8befd85c5c00c09c4f3a1762587707
Kathy <kumqwat@gmail.com>
2021-01-24 15:50:26 -0500

    refactor to update Book components shown according to input

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class SearchBooks extends Component {
     state = {
-        query: ''
+        query: '',
+        results: []
     }
+
+    searchResults = (q) => BooksAPI.search(q)
+        .then((search_results)=> 
+            this.setState(() =>(
+                {
+                    results: search_results
+                }
+            ))
+        ) 
     render() {
+        const { books } = this.props
+        const { query, results } = this.state
         return(
             <div className="search-books">
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
+                <input 
+                    type="text" 
+                    placeholder="Search by title or author"
+                    value={query}
+                    onChange={(event)=>this.searchResults(event.target.value)}
+                    />
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                  {results.map((result) => (
+                      <Book
+                        title = {result.title}
+                        author = {result.author}
+                        image = {result.image}
+                        curShelf = {books.find((book) => book.title === result.title).curShelf}
+                      ></Book>
+                  ))}
+              </ol>
             </div>
           </div>
         )
