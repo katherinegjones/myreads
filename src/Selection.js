818c093e29c3f7113292d64214aed036ee4ca0af
Kathy <kumqwat@gmail.com>
2021-01-25 11:03:52 -0500

    add proptypes functionality for updating books based on selection

--- src/Selection.js
+++ src/Selection.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-
+import { PropTypes } from 'prop-types'
 class Selection extends Component {
+    static propTypes = {
+        thisShelf: PropTypes.string
+    }
+    
+    handleSelect = (event) => {
+        this.props.bookUpdate(event.target.value)
+    }
     render() {
+        const {  thisShelf } = this.props
+
         return(
             <div className="book-shelf-changer">
-            <select>
+            <select value={thisShelf} onChange={this.handleSelect}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
