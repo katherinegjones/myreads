8de6af38ae62d914c8e8e3ff70112ecb3ee53dca
Kathy <kumqwat@gmail.com>
2021-01-25 11:01:33 -0500

    add prop-types, functionality for searching books

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types'
 
 class SearchBooks extends Component {
     state = {
@@ -8,40 +10,70 @@ class SearchBooks extends Component {
         results: []
     }
 
-    searchResults = (q) => BooksAPI.search(q)
-        .then((search_results)=> 
-            this.setState(() =>(
-                {
-                    results: search_results
-                }
-            ))
-        ) 
+    static propTypes = {
+        books: PropTypes.array,
+        updateShelf: PropTypes.func,
+        remove: PropTypes.func
+
+    }
+
+    handleChange = (query) => {
+        this.setState(() => ({
+            query,
+        }))
+
+        setTimeout(() => {
+            this.searchResults()
+        }, 500)
+    }
+    //found method for handling input in the accepted answer here: https://knowledge.udacity.com/questions/293047
+    searchResults = () => {
+        const query = this.state.query
+
+        query === ''
+            ? this.setState(() => ({results: []}))
+            : BooksAPI.search(query)
+        .then((results)=> {
+            if (results.length > 0 && !results.error){
+                this.setState(() => ({
+                    results
+                }))
+            }
+            else this.setState(() => ({results: []}))
+        }
+        )
+            } 
     render() {
-        const { books } = this.props
+        const { books, updateShelf, remove } = this.props
         const { query, results } = this.state
         return(
             <div className="search-books">
             <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <Link className="close-search" to='/'>Close</Link>
               <div className="search-books-input-wrapper">
                 <input 
                     type="text" 
                     placeholder="Search by title or author"
                     value={query}
-                    onChange={(event)=>this.searchResults(event.target.value)}
+                    onChange={(event)=>this.handleChange(event.target.value)}
                     />
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-                  {results.map((result) => (
-                      <Book
-                        title = {result.title}
-                        author = {result.author}
-                        image = {result.image}
-                        curShelf = {books.find((book) => book.title === result.title).curShelf}
-                      ></Book>
+                    {results.map((result, index) => (
+                        <Book
+                                key={index}
+                                id={result.id}
+                                title = {result.title}
+                                author = {[result.authors]}
+                                image = {result.imageLinks !== undefined ? result.imageLinks.thumbnail : null}
+                                curShelf = {books.find((book) => book.title === result.title) ? books.find((book) => book.title === result.title).shelf : 'none'}
+                                moveBook = {updateShelf}
+                                remove = {remove}
+                        />
                   ))}
+                  
               </ol>
             </div>
           </div>
