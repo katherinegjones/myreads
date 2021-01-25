2caa7d2488bbc7f8baa1e90017902b50f74dad3d
Kathy <kumqwat@gmail.com>
2021-01-25 10:57:43 -0500

    add prop-types, add functionality for updating books

--- src/Book.js
+++ src/Book.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
+//import { update } from './BooksAPI'
 import Selection from './Selection'
+import { PropTypes } from 'prop-types'
 
 class Book extends Component {
+    static propTypes = {
+        author: PropTypes.string,
+        title: PropTypes.string,
+        image: PropTypes.string,
+        curShelf: PropTypes.string
+    }
+    
+    handleChange = (value) => {
+            if (value!=='none'){
+                this.props.moveBook({id: `${this.props.id}`}, value)
+            }
+            else {this.props.removeBook(this.props.title)}
+    }
     
     render() {
         const { author, title, image, curShelf } = this.props 
         return(
-        <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
-            <Selection thisShelf={curShelf}/>
-          </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{author}</div>
-      </div>
+            <div className="book">
+            <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
+                <Selection 
+                    thisShelf={curShelf}
+                    bookUpdate={this.handleChange} 
+                />
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{author.join(', ')}</div>
+        </div>
       )
     }
 }
